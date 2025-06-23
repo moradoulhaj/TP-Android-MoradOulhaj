3900cd92e0c34910e6386cb28b58a2ca5644dfb6
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-23 03:47:30 +0100

    protecting the routes login and signup from authetinfied users

--- app/src/main/java/com/example/tpandroidapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/tpandroidapp/ui/auth/AuthViewModel.kt
@@ -2,22 +2,26 @@ package com.example.tpandroidapp.ui.auth
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tpandroidapp.data.datastore.UserPreferences
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-class AuthViewModel : ViewModel() {
+@HiltViewModel
+class AuthViewModel @Inject constructor(
+    private val userPreferences: UserPreferences
+) : ViewModel() {
 
-    // Etat de connexion (false = pas connecté, true = connecté)
-    private val _isLoggedIn = MutableStateFlow(false)
-    val isLoggedIn: StateFlow<Boolean> = _isLoggedIn
-
-    // Simule la connexion : appeler après login réussi
-    fun login() {
-        _isLoggedIn.value = true
-    }
-
-    // Simule la déconnexion
-    fun logout() {
-        _isLoggedIn.value = false
-    }
+    val isLoggedIn = userPreferences.userFlow
+        .map { userData -> !userData?.token.isNullOrEmpty() }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            false
+        )
 }
