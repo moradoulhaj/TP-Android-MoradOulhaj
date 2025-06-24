84e954a0931d4f4bd651500ae01e7d83abc6587f
moradoulhaj <moradoulhaj.off@gmail.com>
2025-06-24 01:11:50 +0100

    adding the cart model and repository

---
+++ app/src/main/java/com/example/tpandroidapp/data/model/Cart.kt
@@ -0,0 +1,13 @@
+package com.example.tpandroidapp.data.model
+
+data class Cart(
+    val id: Int,
+    val idUser: Int,
+    val idProduct: Int,
+    val nameProduct: String,
+    val priceProduct: String,
+    val count: Int,
+    val img: String,
+    val createdAt: String,
+    val updatedAt: String
+)
